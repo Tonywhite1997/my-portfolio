dec5bf8a2387831e0e2ca46201fd2d6c1712caba
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2024-05-29 14:29:50 +0100

    added input and textarea to contact so as to receive message from clients.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,26 @@ function Contact({contactRef}) {
       <h2>Shoot me a message.</h2>
       <p>I am currently seeking an opportunity to gain my first job experience as a Software Developer.</p>
       <p className='text'>I'd love to work with you and your team.</p>
-      <a href='mailto:omodaratantony@gmail.com'>Get in touch</a>
+      <form className='form'>
+        <div className='name-email-container'>
+          <div className='container'>
+            <label>Your Name</label>
+            <input type="text" placeHolder="Mark Zukerberg" />
+          </div>
+          <div className='container'>
+            <label>Your Email</label>
+            <input type="email" placeHolder="Mark@Zukerberg.com" />
+          </div>
+          </div>
+          <div className='message-container'>
+            <label>Your Message</label>
+            <textarea className='message' placeholder='Hey, I am Mark and I would love to invite you for an interview' />
+          </div>
+          <div className='button'>
+           <button>Shoot!</button>
+          </div>
+      </form>
+     
     </section>
   )
 }
