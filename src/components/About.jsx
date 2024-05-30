60d1a98406dac28073a47c5783adc6da9e7736be
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2024-05-30 14:47:55 +0100

    revisited the about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ function About({aboutRef}) {
         </div>
         <div className='about-description'>
             <div className='text-des'>
-                <p className='my-des'>Hi there! My name is Omodaratan Oluwole Tony, and I have a BSc in Computer Science. A few years ago, I started on a journey that I never thought I would start, as I had other career paths planned for my future. Fast forward three years, and I’m still passionate about the choice I made to pursue software development. I am confident in my ability to tackle challenges and deliver high-quality work. My dedication and enthusiasm for this field drive me to continually improve and grow as a professional.</p>
+                <p className='my-des'>Hi there! My name is Omodaratan Oluwole Tony. A flexible Software Engineer with a BSc in Computer Science. A few years ago, I started on a journey that I never thought I would start, as I had other career paths planned for my future. Fast forward three years, and I’m still passionate about the choice I made to pursue software development. I am confident in my ability to tackle challenges and deliver high-quality work. My dedication and enthusiasm for this field drive me to continually improve and grow as a professional.</p>
                 <p className='my-des'>When I’m not learning new tech or working on my latest project, you can find me listening to music or playing games.</p>
                 
                 <div className='technologies-container'>
